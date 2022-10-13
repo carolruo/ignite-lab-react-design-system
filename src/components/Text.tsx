531ca0296c0ac5efa22515cc55531c94dd8c2307
Carolina <carolinaruo@gmail.com>
2022-10-13 14:36:34 -0300

    Aprimora component com radix

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,5 +1,28 @@
-export function Text() {
+import { Slot } from '@radix-ui/react-slot';
+import { clsx } from 'clsx';
+import { ReactNode } from 'react';
+
+export interface TextProps {
+    size?: 'sm' | 'md' | 'lg';
+    children: ReactNode;
+    asChild?: boolean;
+}
+
+export function Text({ size = 'md', children, asChild }: TextProps) {
+    const Comp = asChild ? Slot : 'span';
+
     return (
-        <h1>Hello World</h1>
+        <Comp
+            className={clsx(
+                'text-gray-100 font-sans',
+                {
+                    'text-xs': size === 'sm',
+                    'text-md': size === 'md',
+                    'text-lg': size === 'lg',
+                }
+            )}
+        >
+            {children}
+        </Comp>
     )
 }
